cff75a3ce40e0cebd2b2c210dc754dd8011a49bd
Gustavo Paulo <gustavo.psilvaa02@gmail.com>
2022-10-06 22:45:29 -0300

    style for index.html is done. Map page created

---
+++ src/script/index.js
@@ -0,0 +1,17 @@
+let user = { nome: "Cliente", password: "cliente123" };
+
+const usernameInput = document.getElementById("username_login");
+const passwordInput = document.getElementById("password_login");
+const errorSpan = document.getElementById("errMsg");
+
+document.getElementById("button_login").addEventListener("click", (event) => {
+    if (usernameInput.value == user.nome && passwordInput.value == user.password) {
+        window.location.href = "../view/map.html";
+    } else if (usernameInput.value !== user.nome || passwordInput.value !== user.password) {
+        passwordInput.style.border = "2px solid #f20001";
+        usernameInput.style.border = "2px solid #f20001";
+        errorSpan.innerHTML = "Usuário ou senha incorreto(s)";
+        errorSpan.classList.add("error_msg");
+    }
+
+})
