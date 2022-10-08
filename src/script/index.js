2b07c8e5737a1655a51acac66aa741ac768ad338
Gustavo Paulo <gustavo.psilvaa02@gmail.com>
2022-10-08 17:34:56 -0300

    Map Waze initialized and vibration logic idea

--- src/script/index.js
+++ src/script/index.js
@@ -6,7 +6,7 @@ const errorSpan = document.getElementById("errMsg");
 
 document.getElementById("button_login").addEventListener("click", (event) => {
     if (usernameInput.value == user.nome && passwordInput.value == user.password) {
-        window.location.href = "../view/map.html";
+        window.location.href = "../view/home.html";
     } else if (usernameInput.value !== user.nome || passwordInput.value !== user.password) {
         passwordInput.style.border = "2px solid #f20001";
         usernameInput.style.border = "2px solid #f20001";
